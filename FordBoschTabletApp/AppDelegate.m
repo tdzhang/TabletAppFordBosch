15e94bab25a679ccd2d597efef0a4bf0aa6476e4
Johan Ismael <johanismael@gmail.com>
2013-10-24 15:49:50 -0700

    udp and first screen

--- FordBoschTabletApp/AppDelegate.m
+++ FordBoschTabletApp/AppDelegate.m
@@ -43,4 +43,14 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"boom"
+                                                        message:notification.alertBody
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+    [alertView show];
+}
+
 @end
--- FordBoschTabletApp/ViewController.m
+++ FordBoschTabletApp/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "GCDAsyncUdpSocket.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) GCDAsyncUdpSocket *udpSocket;
 
 @end
 
@@ -17,13 +20,45 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.udpSocket = [[GCDAsyncUdpSocket alloc] initWithDelegate:self
+                                                   delegateQueue:dispatch_get_main_queue()];
+    
+    NSError *error = nil;
+    [self.udpSocket bindToPort:5060
+                    error:&error];
+    
+    if (error) {
+        NSLog(@"%@", [error localizedDescription]);
+    } else {
+        [self.udpSocket beginReceiving:&error];
+        
+        if (error) {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+    }
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)sendData:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSData *data = [@"bonjour" dataUsingEncoding:NSUTF8StringEncoding];
+    [self.udpSocket sendData:data
+                      toHost:@"10.34.128.149"
+                        port:5060
+                 withTimeout:3
+                         tag:0];
+}
+
+- (void)udpSocket:(GCDAsyncUdpSocket *)sock
+   didReceiveData:(NSData *)data
+      fromAddress:(NSData *)address
+withFilterContext:(id)filterContext
+{
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    notification.alertBody = [[NSString alloc] initWithData:data
+                                                   encoding:NSUTF8StringEncoding];
+    
+    [[UIApplication sharedApplication] presentLocalNotificationNow:notification];
 }
 
 @end
